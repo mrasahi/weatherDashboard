1c738bba5fe575625f98f748087e415f68be17fc
Mak <makotorasahi@gmail.com>
2020-07-31 18:37:12 -0700

    index page setup

---
+++ assets/js/weatherapp.js
@@ -0,0 +1,8 @@
+// Weather api
+// https://openweathermap.org/api
+// api key
+// 204a21677e88c64a7fde4e4dce4f596a
+
+
+// example
+// https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=1dd25ac798a84daed3b612ef4b3c9a3e
